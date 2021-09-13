892887885b4cb203ebd9ce4f69b867ec1c13c3ac
Andrew M. Sweeris <46544596+AndrewSweeris@users.noreply.github.com>
2021-09-12 22:03:24 -0500

    Updated files via upload
    Updated files Anagrams.java and AnagramMain.java to updated standards, increasing functionality to completion.
    
     - AnagramMain.java lacks testcases

--- 06.recursion/5.recursion_Anagram/Anagrams.java
+++ 06.recursion/5.recursion_Anagram/Anagrams.java
@@ -1,78 +1,108 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * A class that generates anagrams of a word that can be found in a provided
+ * dictionary.
  *
- * @author sweer
+ * @author Andrew Sweeris
  */
 public class Anagrams {
 
-    private List<String> dictionary;
-    private List<String> prunedDictionary;
+	private List<String> dictionary;
+	private List<String> prunedDictionary;
+	private List<String> used;
 
-    /**
-     * Constructor method for class that takes argument for a List of Strings
-     * and then instantiates the object
-     *
-     * @param dictionary dictionary argument
-     */
-    public Anagrams(List<String> dictionary) {
-        this.dictionary = dictionary;
-    }
+	/**
+	 * Constructor method for class that takes argument for a List of Strings and
+	 * then instantiates the object
+	 *
+	 * @param dictionary dictionary argument
+	 */
+	public Anagrams(List<String> dictionary) {
+		this.dictionary = dictionary;
+		prunedDictionary = new ArrayList<String>();
+		used = new ArrayList<String>();
+	}
 
-    /**
-     * Function that prints anagrams from input text
-     *
-     * @param text the text to prune anagrams from
-     * @param max the maximum amount of anagrams to be made. 0 is unlimited
-     */
-    public void print(String text, int max) {
-        if (max < 0) {
-            throw new IllegalArgumentException("Max < 0");
-        }
-        LetterInventory textInv = new LetterInventory(text);
-        pruner(textInv, 0);
-        anagramGenerator(textInv, 0, max);
-    }
+	/**
+	 * Function that prints anagrams from input text
+	 *
+	 * @param text the text to prune anagrams from
+	 * @param max  the maximum amount of anagrams to be made. 0 is unlimited
+	 */
+	public void print(String text, int max) {
+		if (max < 0) {
+			throw new IllegalArgumentException("Max < 0");
+		}
+		LetterInventory textInv = new LetterInventory(text);
+		pruner(textInv, 0);
+		anagramGenerator(textInv, 0, 0, max);
+		prunedDictionary.clear();
+		used.clear();
+	}
 
-    /**
-     * Function that prunes impossible words from dictionary
-     *
-     * @param textInv the LetterInventory to check against the dictionary
-     * @param i the current index
-     */
-    private void pruner(LetterInventory textInv, int i) {
-        if (i < dictionary.size()) {
-            LetterInventory dictInv = new LetterInventory(dictionary.get(i));
-        }
-    }
+	/**
+	 * Function that prunes impossible words from dictionary
+	 *
+	 * @param textInv the LetterInventory to check against the dictionary
+	 * @param i       the current index
+	 */
+	private void pruner(LetterInventory textInv, int i) {
+		if (i < dictionary.size()) {
+			LetterInventory dictInv = new LetterInventory(dictionary.get(i));
+			if (i < dictionary.size()) {
+				if (textInv.subtract(dictInv) != null) {
+					prunedDictionary.add(dictionary.get(i));
+				}
+				pruner(textInv, i + 1);
+			}
+		}
+	}
 
-    /**
-     * Function that generates anagrams from LetterInventory textInv, and then
-     * calls function to subtract letters used from textInv. Generates all
-     * possible anagrams within the anagram maximum.
-     *
-     * @param textInv LetterInventory of remaining, usable characters
-     * @param i Current index in dictionary
-     * @param max Maximum amount of anagrams
-     */
-    private void anagramGenerator(LetterInventory textInv, int i, int max) {
-        if (i < dictionary.size() && (i < max || max == 0) && !textInv.isEmpty()) {
-            LetterInventory remInv = new LetterInventory(dictionary.get(i));
-        }
-    }
+	/**
+	 * Method that generates all possible anagrams by parsing through both
+	 * prunedDictionary and used Lists to check if word has already been used and
+	 * can be used.
+	 * 
+	 * @param textInv Current letters to use
+	 * @param i       Current index
+	 * @param count   Current count of used words
+	 * @param max     Maximum amount of words to use
+	 */
+	private void anagramGenerator(LetterInventory textInv, int i, int count, int max) {
+		if (i >= prunedDictionary.size() || (count == max && max != 0) || textInv.isEmpty()) {
+			if (textInv.isEmpty()) {
+				System.out.println(usedString(0));
+			}
+		} else {
+			LetterInventory dictInv = new LetterInventory(prunedDictionary.get(i));
+			LetterInventory tempInv = textInv.subtract(dictInv);
+			List<String> tempList = used;
+			if (tempInv != null && !tempList.contains(prunedDictionary.get(i))) {
+				String tempCurrent = "";
+				used.add(prunedDictionary.get(i));
+				anagramGenerator(tempInv, 0, count, max);
+				used.remove(used.size() - 1);
+				anagramGenerator(textInv, i + 1, count + 1, max);
 
-    /**
-     * Function that removes used characters from textInv, and then returns
-     * remaining unused characters.
-     *
-     * @param textInv Characters remaining
-     * @param remInv Characters remaining to be removed
-     */
-    private LetterInventory characterRemover(LetterInventory textInv, LetterInventory remInv) {
-        if (textInv.isEmpty() && !remInv.isEmpty()) {
-            throw new Error("textInv empty before remImv");
-        }
-        return textInv;
-    }
+			} else {
+				anagramGenerator(textInv, i + 1, count, max);
+			}
+		}
+	}
+
+	/**
+	 * Method that prints out Strings in used List
+	 * 
+	 * @param i current index
+	 * @return List variables
+	 */
+	private String usedString(int i) {
+		if (i < used.size()) {
+			return used.get(i) + " " + usedString(i + 1);
+		}
+		return "";
+	}
 }
